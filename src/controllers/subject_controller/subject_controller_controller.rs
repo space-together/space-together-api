a041fb75291abcae4c7f7bfe46c1a14ed12938ff
brunorwanda4 <brunorwanda4@gmail.com>
2025-01-15 16:40:59 +0200

    get class room by id error

--- src/controllers/subject_controller/subject_controller_controller.rs
+++ src/controllers/subject_controller/subject_controller_controller.rs
@@ -1,9 +1,13 @@
-use std::sync::Arc;
+use std::{str::FromStr, sync::Arc};
 
 use mongodb::bson::oid::ObjectId;
 
 use crate::{
-    error::db_class_error::DbClassResult,
+    controllers::class_controller::{
+        class_room_controller::get_class_room_by_id,
+        class_room_type_controller::get_class_room_type_by_id,
+    },
+    error::db_class_error::{DbClassError, DbClassResult},
     models::subject_model::subject_model_model::{
         SubjectModel, SubjectModelGet, SubjectModelNew, SubjectModelPut,
     },
@@ -14,6 +18,46 @@ pub async fn create_subject(
     state: Arc<AppState>,
     subject: SubjectModelNew,
 ) -> DbClassResult<SubjectModelGet> {
+    if let Some(class_room) = subject.room_id.clone() {
+        let id = ObjectId::from_str(&class_room);
+        if id.is_err() {
+            return Err(DbClassError::OtherError {
+                err: format!("Your class room id is invalid [{}], try other", class_room),
+            });
+        };
+
+        if let Ok(i) = id {
+            let get = get_class_room_type_by_id(state.clone(), i).await;
+            if get.is_ok() {
+                return Err(DbClassError::OtherError {
+                    err: format!(
+                        "your class room id is not found  [{}], please use other class room id",
+                        class_room
+                    ),
+                });
+            }
+        }
+    }
+
+    if let Some(subject_type) = subject.subject_type_id.clone() {
+        let id = ObjectId::from_str(&subject_type);
+        if id.is_err() {
+            return Err(DbClassError::OtherError {
+                err: format!(
+                    "Your subject type id is invalid [{}], try other",
+                    subject_type
+                ),
+            });
+        };
+
+        if let Ok(i) = id {
+            let get = get_class_room_by_id(state.clone(), i).await;
+            if let Err(e) = get {
+                return Err(DbClassError::OtherError { err: e.to_string() });
+            }
+        }
+    }
+
     let create = state
         .db
         .subject
@@ -53,6 +97,46 @@ pub async fn update_subject_by_id(
     id: ObjectId,
     subject: SubjectModelPut,
 ) -> DbClassResult<SubjectModelGet> {
+    if let Some(class_room) = subject.room_id.clone() {
+        let id = ObjectId::from_str(&class_room);
+        if id.is_err() {
+            return Err(DbClassError::OtherError {
+                err: format!("Your class room id is invalid [{}], try other", class_room),
+            });
+        };
+
+        if let Ok(i) = id {
+            let get = get_class_room_type_by_id(state.clone(), i).await;
+            if get.is_ok() {
+                return Err(DbClassError::OtherError {
+                    err: format!(
+                        "your class room id is not found  [{}], please use other class room id",
+                        class_room
+                    ),
+                });
+            }
+        }
+    }
+
+    if let Some(subject_type) = subject.subject_type_id.clone() {
+        let id = ObjectId::from_str(&subject_type);
+        if id.is_err() {
+            return Err(DbClassError::OtherError {
+                err: format!(
+                    "Your subject type id is invalid [{}], try other",
+                    subject_type
+                ),
+            });
+        };
+
+        if let Ok(i) = id {
+            let get = get_class_room_by_id(state.clone(), i).await;
+            if let Err(e) = get {
+                return Err(DbClassError::OtherError { err: e.to_string() });
+            }
+        }
+    }
+
     let _ = state
         .db
         .subject
