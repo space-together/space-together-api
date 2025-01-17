08f657284e1ee49faac20d42a8da08d6fd3f506b
brunorwanda4 <brunorwanda4@gmail.com>
2025-01-17 20:23:06 +0200

    update sector add sector username

--- src/controllers/school_controller/sector_controller.rs
+++ src/controllers/school_controller/sector_controller.rs
@@ -1,9 +1,14 @@
 use std::sync::Arc;
 
-use mongodb::bson::oid::ObjectId;
+use mongodb::{
+    bson::{doc, oid::ObjectId},
+    options::IndexOptions,
+    IndexModel,
+};
 
 use crate::{
-    error::db_class_error::DbClassResult,
+    error::db_class_error::{DbClassError, DbClassResult},
+    libs::functions::characters_fn::is_valid_username,
     models::school_model::sector_model::{
         SectorModel, SectorModelGet, SectorModelNew, SectorModelPut,
     },
@@ -14,16 +19,67 @@ pub async fn create_sector(
     state: Arc<AppState>,
     sector: SectorModelNew,
 ) -> DbClassResult<SectorModelGet> {
+    let index = IndexModel::builder()
+        .keys(doc! {
+        "username" : 1,
+        })
+        .options(IndexOptions::builder().unique(true).build())
+        .build();
+
+    if let Err(err) = state.db.education.collection.create_index(index).await {
+        return Err(DbClassError::OtherError {
+            err: format!(
+                "Can't create education bcs username is leady exit 😡 [{}]😡 ",
+                err
+            ),
+        });
+    }
+
+    if let Some(ref username) = sector.username {
+        if let Err(err) = is_valid_username(username) {
+            return Err(DbClassError::OtherError {
+                err: err.to_string(),
+            });
+        }
+
+        let get_username = get_sector_by_username(state.clone(), username.clone()).await;
+        if get_username.is_ok() {
+            return Err(DbClassError::OtherError {
+                err: format!(
+                    "Username education is ready exit [{}], please try other",
+                    &username
+                ),
+            });
+        }
+    } else {
+        return Err(DbClassError::OtherError {
+            err: "Username is missing".to_string(),
+        });
+    }
+
     let create = state
         .db
         .sector
         .create(SectorModel::new(sector), Some("sector".to_string()))
         .await?;
+    let get = get_sector_by_id(state.clone(), create).await?;
+    Ok(get)
+}
+
+pub async fn get_sector_by_username(
+    state: Arc<AppState>,
+    username: String,
+) -> DbClassResult<SectorModelGet> {
     let get = state
         .db
         .sector
-        .get_one_by_id(create, Some("sector".to_string()))
-        .await?;
+        .collection
+        .find_one(doc! {"username" : &username})
+        .await?
+        .ok_or(DbClassError::OtherError {
+            err: format!("Sector not found by username [{}]", &username),
+        })?;
+
     Ok(SectorModel::format(get))
 }
 
@@ -50,6 +106,23 @@ pub async fn update_sector_by_id(
     id: ObjectId,
     sector: SectorModelPut,
 ) -> DbClassResult<SectorModelGet> {
+    if let Some(ref username) = sector.username {
+        if let Err(err) = is_valid_username(username) {
+            return Err(DbClassError::OtherError {
+                err: err.to_string(),
+            });
+        }
+
+        let get_username = get_sector_by_username(state.clone(), username.clone()).await;
+        if get_username.is_ok() {
+            return Err(DbClassError::OtherError {
+                err: format!(
+                    "Username education is ready exit [{}], please try other",
+                    &username
+                ),
+            });
+        }
+    }
     let _ = state
         .db
         .sector
