e1c64635f30c0bf422f9cb194abd16a2b3e96857
brunorwanda4 <brunorwanda4@gmail.com>
2025-01-22 15:36:10 +0200

    make model for file

--- src/models/file_model/file_model_model.rs
+++ src/models/file_model/file_model_model.rs
@@ -1,84 +1,95 @@
-// use mongodb::bson::{self, oid::ObjectId, DateTime, Document};
-// use serde::{Deserialize, Serialize};
+use std::str::FromStr;
 
-// #[derive(Debug, Deserialize, Serialize, Clone)]
-// pub struct FileModel {
-//     #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
-//     pub id: Option<ObjectId>,
-//     pub name: String,
-//     pub description: Option<String>,
-//     pub created_on: DateTime,
-//     pub updated_on: Option<DateTime>,
-// }
+use mongodb::bson::{self, oid::ObjectId, DateTime, Document};
+use serde::{Deserialize, Serialize};
 
-// #[derive(Debug, Deserialize, Serialize, Clone)]
-// pub struct FileModelGet {
-//     pub id: String,
-//     pub name: String,
-//     pub description: Option<String>,
-//     pub created_on: String,
-//     pub updated_on: Option<String>,
-// }
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct FileModel {
+    #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
+    pub id: Option<ObjectId>,
+    pub src: String,
+    pub file_type_id: ObjectId,
+    pub description: Option<String>,
+    pub created_on: DateTime,
+    pub updated_on: Option<DateTime>,
+}
 
-// #[derive(Debug, Deserialize, Serialize, Clone)]
-// pub struct FileModelNew {
-//     pub name: String,
-//     pub description: Option<String>,
-// }
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct FileModelGet {
+    pub id: String,
+    pub src: String,
+    pub file_type: String,
+    pub description: Option<String>,
+    pub created_on: String,
+    pub updated_on: Option<String>,
+}
 
-// #[derive(Debug, Deserialize, Serialize, Clone)]
-// pub struct FileModelPut {
-//     pub name: Option<String>,
-//     pub description: Option<String>,
-// }
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct FileModelNew {
+    pub src: String,
+    pub file_type: String,
+    pub description: Option<String>,
+}
 
-// impl FileModel {
-//     pub fn new(class_type: FileModelNew) -> Self {
-//         FileModel {
-//             id: None,
-//             name: class_type.name,
-//             description: class_type.description,
-//             created_on: DateTime::now(),
-//             updated_on: None,
-//         }
-//     }
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct FileModelPut {
+    pub src: Option<String>,
+    pub file_type: Option<String>,
+    pub description: Option<String>,
+}
 
-//     pub fn format(class_type: Self) -> FileModelGet {
-//         FileModelGet {
-//             id: class_type.id.map_or("".to_string(), |id| id.to_string()),
-//             name: class_type.name,
-//             description: class_type.description,
-//             created_on: class_type
-//                 .created_on
-//                 .try_to_rfc3339_string()
-//                 .unwrap_or("".to_string()),
-//             updated_on: Some(class_type.updated_on.map_or("".to_string(), |date| {
-//                 date.try_to_rfc3339_string().unwrap_or("".to_string())
-//             })),
-//         }
-//     }
+impl FileModel {
+    pub fn new(file: FileModelNew) -> Self {
+        FileModel {
+            id: None,
+            src: file.src,
+            description: file.description,
+            file_type_id: ObjectId::from_str(&file.file_type).unwrap(),
+            created_on: DateTime::now(),
+            updated_on: None,
+        }
+    }
 
-//     pub fn put(class_type: FileModelPut) -> Document {
-//         let mut doc = Document::new();
-//         let mut is_update = false;
+    pub fn format(file: Self) -> FileModelGet {
+        FileModelGet {
+            id: file.id.map_or("".to_string(), |id| id.to_string()),
+            src: file.src,
+            description: file.description,
+            file_type: file.file_type_id.to_string(),
+            created_on: file
+                .created_on
+                .try_to_rfc3339_string()
+                .unwrap_or("".to_string()),
+            updated_on: Some(file.updated_on.map_or("".to_string(), |date| {
+                date.try_to_rfc3339_string().unwrap_or("".to_string())
+            })),
+        }
+    }
 
-//         let mut insert_if_some = |key: &str, value: Option<bson::Bson>| {
-//             if let Some(v) = value {
-//                 doc.insert(key, v);
-//                 is_update = true;
-//             }
-//         };
+    pub fn put(file: FileModelPut) -> Document {
+        let mut doc = Document::new();
+        let mut is_update = false;
 
-//         insert_if_some("name", class_type.name.map(bson::Bson::String));
-//         insert_if_some(
-//             "description",
-//             class_type.description.map(bson::Bson::String),
-//         );
+        let mut insert_if_some = |key: &str, value: Option<bson::Bson>| {
+            if let Some(v) = value {
+                doc.insert(key, v);
+                is_update = true;
+            }
+        };
 
-//         if is_update {
-//             doc.insert("updated_on", bson::Bson::DateTime(DateTime::now()));
-//         }
+        insert_if_some("src", file.src.map(bson::Bson::String));
+        insert_if_some("description", file.description.map(bson::Bson::String));
+        insert_if_some(
+            "file_type_id",
+            file.file_type
+                .and_then(|id| ObjectId::from_str(&id).ok())
+                .map(bson::Bson::ObjectId),
+        );
 
-//         doc
-//     }
-// }
+        if is_update {
+            doc.insert("updated_on", bson::Bson::DateTime(DateTime::now()));
+        }
+
+        doc
+    }
+}
