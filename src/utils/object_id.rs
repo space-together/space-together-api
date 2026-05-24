755b06f7c69afda740b739de9743aa5b0a4ff1bf
brunorwanda4 <brunorwanda4@gmail.com>
2026-05-25 00:37:30 +0200

    feat: centralize legacy id compatibility

--- src/utils/object_id.rs
+++ src/utils/object_id.rs
@@ -1,9 +1,11 @@
-use mongodb::bson::oid::ObjectId;
+use mongodb::bson::oid::ObjectId as MongoObjectId;
 
 use crate::errors::AppError;
 
+pub type ObjectId = MongoObjectId;
+
 pub fn parse_object_id_value(value: &str) -> Result<ObjectId, AppError> {
     ObjectId::parse_str(value).map_err(|e| AppError {
-        message: format!("Invalid ObjectId: {}", e),
+        message: format!("Invalid ObjectId-compatible ID: {}", e),
     })
 }
